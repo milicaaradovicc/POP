c538def4d45ef525d1ac982ca2d29da99d1fd431
milicaaradovicc <milicaaradovicc@gmail.com>
2024-10-28 02:01:46 +0100

    model uradjen

---
+++ SR31-2023-POP2024/ModelAutomobila.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR31_2023_POP2024
+{
+    public class ModelAutomobila{
+        public MarkaAutomobila Marka { get; set; }
+        public string NazivModela { get; set; }
+
+        public ModelAutomobila(MarkaAutomobila marka, string nazivModela)
+        {
+            Marka = marka;
+            NazivModela = nazivModela;
+        }
+    }
+}
--- SR31-2023-POP2024/obj/Debug/net8.0/SR31-2023-POP2024.AssemblyInfo.cs
+++ SR31-2023-POP2024/obj/Debug/net8.0/SR31-2023-POP2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SR31-2023-POP2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bb12318ac4500526bb3d689f15f41f7cbe184c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SR31-2023-POP2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SR31-2023-POP2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
