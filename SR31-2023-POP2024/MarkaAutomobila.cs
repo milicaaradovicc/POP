5bb12318ac4500526bb3d689f15f41f7cbe184c0
milicaaradovicc <milicaaradovicc@gmail.com>
2024-10-28 01:46:41 +0100

    Prvi commit

---
+++ SR31-2023-POP2024/MarkaAutomobila.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR31_2023_POP2024
+{
+    public class MarkaAutomobila{
+        public string Naziv { get; set; }
+        public string DrzavaNastanka { get; set; }
+
+        public MarkaAutomobila(string naziv, string drzavaNastanka)
+        {
+            Naziv = naziv;
+            DrzavaNastanka = drzavaNastanka;
+        }
+    }
+}
