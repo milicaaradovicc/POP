fbb12b461304864bdc39ade76667c78601eb77f6
Milica Radovic <milicaaradovicc@gmail.com>
2024-12-02 00:34:10 +0100

    WPF dodat

---
+++ WPF/Windows/AddCarWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SR31_2023_POP2024.Repository;
+using SR31_2023_POP2024;
+
+namespace WPF.Windows
+{
+    /// <summary>
+    /// Interaction logic for AddCarWindow.xaml
+    /// </summary>
+    public partial class AddCarWindow : Window
+    {
+        private CarRepository carRepository;
+
+        public AddCarWindow()
+        {
+            InitializeComponent();
+            carRepository = new CarRepository();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var id = carRepository.GetNextCarId(); 
+
+            
+            var marka = new MarkaAutomobila(MarkaTextBox.Text, DrzavaTextBox.Text);
+            var model = new ModelAutomobila(marka, ModelTextBox.Text);
+
+            var automobil = new Automobil(
+                id,
+                marka,
+                model,
+                int.Parse(GodisteTextBox.Text),
+                int.Parse(SnagaTextBox.Text),
+                PogonTextBox.Text
+            );
+
+            carRepository.AddCar(automobil); 
+            MessageBox.Show("Automobil je uspešno dodat!");
+
+            this.Close(); 
+        }
+
+    }
+}
---
+++ WPF/Windows/EditWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SR31_2023_POP2024;
+using SR31_2023_POP2024.Service;
+
+namespace WPF.Windows
+{
+    /// <summary>
+    /// Interaction logic for EditWindowxaml.xaml
+    /// </summary>
+    public partial class EditWindow : Window
+    {
+        private readonly CarService _carService;  
+        private Automobil selectedCar;  
+
+        // Konstruktor
+        public EditWindow(Automobil car, CarService carService)
+        {
+            InitializeComponent();
+            selectedCar = car;  
+            _carService = carService; 
+            this.DataContext = selectedCar;  
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            _carService.EditCar(selectedCar.ID.ToString(), selectedCar); 
+            MessageBox.Show("Uspešno ste izmenili automobil!");  
+            this.Close();  
+        }
+    }
+}
